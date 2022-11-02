d30541c0d1362b6298a3535239807d1b3e808362
xxalison <109158990+xxalisonn@users.noreply.github.com>
2022-11-02 20:54:21 +0800

    Update tool.py

--- tool.py
+++ tool.py
@@ -12,3 +12,11 @@ def save_dic_tensor(json_name, save_name):
     di = json.dumps(json_name,sort_keys=False,indent=4,separators=(',',':'))
     with open('./rel/'+str(save_name)+'.json','w') as f:
         f.write(di)
+        
+def save_tensor(json_name, save_name):
+    for key in json_name.keys():
+        json_name[key] = json_name[key].tolist()
+    di = json.dumps(json_name,sort_keys=False,indent=4,separators=(',',':'))
+    with open('./rel/'+str(save_name)+'.json','w') as f:
+        f.write(di)
+
